734adc8426a4ac682074f42891fdda720c6ed86b
Tiago Melo Menezes De Brito <tiagoinvestsalvador@gmail.com>
2023-12-12 21:37:42 -0300

    Adding js to mini project 29

---
+++ 4Aditionalprojects/29-confy-store/cart/setupCart.js
@@ -0,0 +1,76 @@
+// import
+import {
+  getStorageItem,
+  setStorageItem,
+  formatPrice,
+  getElement,
+} from "../utils.js";
+import { openCart } from "./toggleCart.js";
+import { findProduct } from "../store.js";
+import addToCartDOM from "./addToCartDOM.js";
+// set items
+
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
+
+export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id === id);
+
+  if (!item) {
+    let product = findProduct(id);
+    // add item to the the
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    // add item to the DOM;
+    addToCartDOM(product);
+  } else {
+    // update values
+    const amount = increaseAmount(id);
+    const items = [...cartItemsDOM.querySelectorAll(".cart-item-amount")];
+    const newAmount = items.find((value) => value.dataset.id === id);
+    newAmount.textContent = amount;
+  }
+  // add one to the item count
+  displayCartItemCount();
+  // display cart totals
+  displayCartTotal();
+  // set cart in local storage
+
+  setStorageItem("cart", cart);
+  //more stuff coming up
+  openCart();
+};
+function displayCartItemCount() {
+  const amount = cart.reduce((total, cartItem) => {
+    return (total += cartItem.amount);
+  }, 0);
+  cartItemCountDOM.textContent = amount;
+}
+function displayCartTotal() {
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  cartTotalDOM.textContent = `Total : ${formatPrice(total)} `;
+}
+function displayCartItemsDOM() {
+  cart.forEach((cartItem) => {
+    addToCartDOM(cartItem);
+  });
+}
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id);
+}
+function increaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1;
+      cartItem = { ...cartItem, amount: newAmoun };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
